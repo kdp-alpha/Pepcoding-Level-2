1f047eedeb671eaadd6d8c20dba2f7f8946835e6
karan pawar <85121133+kdp-alpha@users.noreply.github.com>
2022-07-16 19:57:57 +0530

    Update 1.Graph_Representation.java

--- Graphs/1.Graph_Representation.java
+++ Graphs/1.Graph_Representation.java
@@ -37,3 +37,44 @@ class Main {
       printGraph(adj);
     }
 }
+
+
+
+//2. this one easy 
+import java.util.*;
+class Main {
+   public static void addEdge(ArrayList<Integer>[] graph,int u,int v){
+       graph[u].add(v);
+       graph[v].add(u);
+   }
+   
+  public static void printGraph(ArrayList<Integer>[] graph){
+        for(int i=0;i<graph.length;i++){
+            System.out.print("edge of vertex " + i + " :");
+            for(int nbr:graph[i]){
+                System.out.print("->" + nbr);
+            }
+            System.out.println();
+        }
+  }
+        
+    public static void main(String[] args){
+      int V = 7;
+      ArrayList<Integer>[] graph = new ArrayList[V];
+      
+      for(int i=0;i<V;i++){
+          graph[i] = new ArrayList<>();
+      }
+      
+      addEdge(graph,0,1);
+      addEdge(graph,0,3);
+      addEdge(graph,1,2);
+      addEdge(graph,2,3);
+      addEdge(graph,3,4);
+      addEdge(graph,4,5);
+      addEdge(graph,5,6);
+      addEdge(graph,4,6);
+      
+      printGraph(graph);
+    }
+}
